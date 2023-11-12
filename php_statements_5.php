f04edb032ff8a3e2d3b6eabbdf44ac53680460e3
RexPacificusVII <110966402+RexPacificusVII@users.noreply.github.com>
2023-11-12 23:23:24 +0800

    Format answer into a table

--- php_statements_5.php
+++ php_statements_5.php
@@ -4,21 +4,42 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Statements Exercise 5</title>
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    th, td {
+      border: 1px solid black;
+      text-align: center;
+      padding: 5px;
+    }
+  </style>
 </head>
 <body>
 
   <?php
-
   echo "Division Table:<br>";
-
+  
+  echo "<table>";
+  echo "<thead><tr><th></th>";
   for ($i = 1; $i <= 10; $i++) {
-      for ($j = 1; $j <= 10; $j++) {
-          $result = $i / $j;
-          echo "$i / $j = " . number_format($result, 2) . "\t";
-      }
-      echo "<br>";
+    echo "<th>{$i}</th>";
   }
+  echo "</tr></thead>";
 
+  echo "<tbody>";
+  for ($i = 1; $i <= 10; $i++) {
+    echo "<tr><th>{$i}</th>";
+    for ($j = 1; $j <= 10; $j++) {
+      $quotient = $i / $j;
+      $roundedQuotient = round($quotient, 2);
+      echo "<td>{$roundedQuotient}</td>";
+    }
+    echo "</tr>";
+  }
+  echo "</tbody>";
+  echo "</table>";
   ?>
 
 </body>
